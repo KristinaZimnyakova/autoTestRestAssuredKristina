99f49417718eaf655c10731c6eb1525c28a481dc
79376 <kris19932311@gmail.com>
2023-04-02 23:05:24 +0400

    7 ДЗ (c валидацией) параметр-ые доделать с циклом

--- src/test/java/bookingMethods/CreateBooking.java
+++ src/test/java/bookingMethods/CreateBooking.java
@@ -5,6 +5,8 @@ import io.restassured.specification.RequestSpecification;
 import models.Booking;
 import services.LoginService;
 
+import static org.hamcrest.Matchers.equalTo;
+
 public class CreateBooking {
 
 
@@ -13,6 +15,13 @@ public class CreateBooking {
         Integer bookingid = session.body(booking)
                 .when().post("/booking")
                 .then().statusCode(200)
+                .body("booking.firstname", equalTo(booking.getFirstname()))
+                .body("booking.lastname", equalTo(booking.getLastname()))
+                .body("booking.totalprice", equalTo(booking.getTotalprice()))
+                .body("booking.depositpaid", equalTo(booking.getDepositpaid()))
+                .body("booking.additionalneeds", equalTo(booking.getAdditionalneeds()))
+                .body("booking.bookingdates.checkin", equalTo(booking.getBookingdates().getCheckin()))
+                .body("booking.bookingdates.checkout", equalTo(booking.getBookingdates().getCheckout()))
                 .extract().body().jsonPath().get("bookingid");
         return bookingid;
     }
